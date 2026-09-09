39636ef5c95ccb632c6c7281b9bee043bda54219
Camilo Hernandez <c.aggrometal@gmail.com>
2026-09-09 20:19:20 -0300

    fix(andrew): boot through root application shell

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import EditorApp from './editor/EditorApp';
-import './editor/editor.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Root element not found');
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Root element not found");
 
 ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode><EditorApp /></React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
