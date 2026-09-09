3979d6b5b26cf2b80a865c30c2f625ed353b7239
Camilo Hernandez <c.aggrometal@gmail.com>
2026-09-08 23:12:07 -0300

    Avoid editor alias dependency in entrypoint

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import EditorApp from '@editor/EditorApp';
-import '@editor/editor.css';
+import EditorApp from './editor/EditorApp';
+import './editor/editor.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
