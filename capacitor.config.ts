ef5e1b1d61f9c7262c4cf2e837b0ea435c19ae33
Camilo Hernandez <c.aggrometal@gmail.com>
2026-09-10 03:34:15 -0300

    feat: configure Capacitor Android app

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.andrew.ai',
+  appName: 'Andrew 2.0',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
