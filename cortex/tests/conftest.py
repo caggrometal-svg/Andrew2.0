580e4489833296dc7731021025ed40a3b5da1557
Camilo Hernandez <c.aggrometal@gmail.com>
2026-09-11 12:09:21 -0300

    cortex: provide isolated test configuration

--- cortex/tests/conftest.py
+++ cortex/tests/conftest.py
@@ -1,3 +1,11 @@
+import os
+
+os.environ.setdefault("CORTEX_SHARED_TOKEN", "test-token")
+os.environ.setdefault("REDIS_URL", "redis://localhost:6379/0")
+os.environ.setdefault("QDRANT_URL", "http://localhost:6333")
+os.environ.setdefault("LITELLM_BASE_URL", "http://localhost:4000")
+os.environ.setdefault("LITELLM_MASTER_KEY", "test-key")
+
 import pytest
 
 from app.admission import AdmissionController, Priority
