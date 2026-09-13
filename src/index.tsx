eff3ddd6443d5c54d8268e0b928c06eff59a957d
Camilo Hernandez <c.aggrometal@gmail.com>
2026-09-13 20:46:06 -0300

    fix(ui): wire runtime error recovery boundary

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@ui/App';
+import RuntimeErrorBoundary from '@ui/RuntimeErrorBoundary';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -9,6 +10,8 @@ if (!rootElement) {
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <RuntimeErrorBoundary>
+      <App />
+    </RuntimeErrorBoundary>
   </React.StrictMode>
 );
